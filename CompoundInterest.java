092e7de7e4b8ed72033e3742b6367537439cf939
Sanjithj <sanjithj591997@gmail.com>
2022-06-24 15:14:48 +0530

    added :first commit

---
+++ CompoundInterest.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class CompoundInterest {
+	
+	
+		
+		public static void main(String[] args) {
+			
+			double p,t,r,n;
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the principle");
+			p = sc.nextDouble();
+			
+			System.out.println("Enter the time");
+			t = sc.nextDouble();
+		
+			System.out.println("Enter the rate");
+			r = sc.nextDouble();
+			r =r/100;
+			
+			System.out.println("Enter the number of times ci per year");
+			n = sc.nextDouble();
+			
+			double Amount= p *Math.pow (1 + (r /n ), n * t);
+			double ci = Amount - p;
+			System.out.println("Comp interest: " + ci );
+			System.out.println("Amount:  " + Amount);
+		
+	}
+}
